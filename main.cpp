17fc1cca18aae8fd5f534859e56d4a678cd0be0e
yl1025 <43146621+yl1025@users.noreply.github.com>
2018-09-23 23:51:58 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,176 @@
-#include <iostream>
-#include "problem4a.h"
+#include<iostream>
+#include"LinkedList.h"
+using namespace std;
+linkedList::linkedList()
+{
+    head = NULL;
+}
 
+void linkedList::Push(int data)
+
+{
+    node *newNode,*tmp;
+    newNode = new node;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+
+    else
+
+    {
+        tmp = head;
+        head = newNode;
+        head->next = tmp;
+
+    }
+}
+
+void linkedList::Pop()
+
+{
+    node *tmp = head;
+    head = head->next;
+    delete tmp;
+}
+
+int linkedList::Peek()
+
+{
+
+    return head->data;
+
+}
+
+void linkedList::insertionSort()
+
+{
+
+    if (head == NULL || head->next == NULL)
+
+        return ;
+
+    node *newNode = new node;
+
+    newNode->data = head->data;
+
+    newNode->next = NULL;
+
+    node *cur = head->next;
+
+    while (cur != NULL)
+    {
+        node *pointer1 = newNode;
+
+        node *next = cur->next;
+
+        if (cur->data <= newNode->data)
+        {
+            node *head1 = newNode;
+
+            newNode = cur;
+
+            newNode->next = head1;
+        }
+        else
+        {
+            while (pointer1->next != NULL)
+
+            {
+                if (cur->data > pointer1->data && cur->data <= pointer1->next->data)
+
+                {
+
+                    node *next1= pointer1->next;
+
+                    pointer1->next = cur;
+
+                    cur->next = next1;
+
+                }
+
+                pointer1 = pointer1->next;
+
+            }
+
+            if (pointer1->next == NULL && cur->data > pointer1->data)
+
+            {
+
+                pointer1->next = cur;
+
+                cur->next = NULL;
+
+            }
+
+        }
+        cur = next;
+
+    }
+
+    head= newNode;
+}
+void linkedList::print()
+{
+    node *cur = head;
+    while (cur != NULL)
+    {
+
+        cout << cur->data << " " << endl;
+
+        cur = cur->next;
+
+    }
+    cout << endl;
+}
+int linkedList::size()
+{
+    node *cur = head;
+    int count = 0;
+    while (cur != NULL)
+    {
+        count++;
+
+        cur = cur->next;
+
+    }
+    return count;
+}
 int main()
 {
-    string string1 = "what the?";
-    ProblemFourA(string1);
-    cout << string1;
-    return 0;
+    linkedList list;
+
+    list.Push(10);
+
+    list.Push(4);
+
+    list.Push(14);
+
+    list.Push(9);
+
+    list.Push(3);
+
+    cout << "Before sorting: " << endl;
+
+    list.print();
+
+    list.insertionSort();
+
+    cout << "After sorting: " << endl;
+
+    list.print();
+
+    cout << "Peek: "<<list.Peek()<<" \ndata "<< list.Peek()<<" is popped" << endl;
+
+    list.Pop();
+
+    cout << "Peek: "<<list.Peek()<<"\ndata "<<list.Peek()<<" is popped" << endl;
+
+    list.Pop();
+
+    cout << "Peek: " << list.Peek() << endl;
+
 }
---
+++ sort.h
@@ -0,0 +1,8 @@
+//
+// Created by wwwyu on 9/23/2018.
+//
+
+#ifndef PM2HW1P3_SORT_H
+#define PM2HW1P3_SORT_H
+
+#endif //PM2HW1P3_SORT_H
